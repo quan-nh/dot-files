00ca44aeac430b58abeecf410b41c76dc5d422d9
Quan Nguyen <huu2uan@gmail.com>
2018-06-25 15:34:01 +0700

    Update lein profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,9 @@
-{:user {:plugins [[lein-ancient "0.6.12"]
-                  [lein-cljfmt "0.5.7"]
-                  [lein-nsorg "0.1.4"]
-                  [walmartlabs/vizdeps "0.1.6"]]}}
+{:user {:plugins [[lein-ancient "0.6.15"]
+                  [lein-cljfmt "0.5.6"]
+		  [lein-nsorg "0.1.4"]
+		  [walmartlabs/vizdeps "0.1.6"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.17.0"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.13"]
+                       [cider/piggieback "0.3.5"]
+                       [figwheel-sidecar "0.5.16"]]
+        :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}}
