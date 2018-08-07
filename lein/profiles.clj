29f54050989861e451e6d4116b68fc02c62ff037
Quan Nguyen <huu2uan@gmail.com>
2018-08-07 22:01:59 +0700

    lein

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[lein-ancient "0.6.15"]
                   [lein-cljfmt "0.6.0"]
-		              [lein-nsorg "0.2.0"]
-		              [walmartlabs/vizdeps "0.1.6"]]}}
+                  [lein-nsorg "0.2.0"]
+                  [walmartlabs/vizdeps "0.1.6"]]}}
