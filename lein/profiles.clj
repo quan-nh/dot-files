64104627bf3e618da777216e0dc296632584d187
Quan Nguyen <huu2uan@gmail.com>
2020-05-21 15:04:16 +0700

    lein profile update

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,7 @@
-{:user {:plugins [[lein-ancient "0.6.15"]
+{:user
+ {:plugins [[lein-ancient "0.6.15"]
                   [lein-cljfmt "0.6.7"]
                   [lein-nsorg "0.3.0"]
-                  [walmartlabs/vizdeps "0.2.0"]]}}
+                  [walmartlabs/vizdeps "0.2.0"]]}
+ {:dependencies [[hashp "0.1.1"]]
+  :injections [(require 'hashp.core)]}}
